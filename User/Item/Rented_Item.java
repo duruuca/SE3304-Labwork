ed0a2c331fd5ff2ed8d326c71c1e09ed09f2dcc5
duruuca <149398831+duruuca@users.noreply.github.com>
2024-05-03 12:59:43 +0300

    Item files

---
+++ User/Item/Rented_Item.java
@@ -0,0 +1,11 @@
+package Item;
+
+public class Rented_Item {
+
+	private Date DueDate;
+	private String ID;
+	private String Details;
+	private String AvailabilityStatus;
+	private String Type;
+
+}
